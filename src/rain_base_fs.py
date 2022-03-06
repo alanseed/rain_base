2767fe8a66067e54a390c8de27f401b4264642eb
alan.seed <alan.seed@gmail.com>
2022-03-06 17:54:06 +1100

    test writing the files

--- src/rain_base_fs.py
+++ src/rain_base_fs.py
@@ -16,9 +16,23 @@ def get_field_stats(buf):
     rain_area = (rain > 0.1).sum()
     valid_area = rain.count()
     war = 100 * rain_area / valid_area 
+    
     return mean, std, war 
 
-def write_to_rain_basefs(rf3_name): 
+# Write the data to the MongoDB
+# returns None on error 
+def write_to_rain_basefs(**kwargs): 
+    rf3_name = kwargs.get("file_path",None)
+    db_client = kwargs.get("db_client",None)
+    db_name = kwargs.get("db_name", None) 
+    if db_client == None and db_name == None:
+        print("Need one of db_name or db_client")
+        return None
+
+    if rf3_name ==  None: 
+        print(f"file_path keyword not found")
+        return None
+
     # read the ncfile into memory 
     try:
         nc_file = open(rf3_name,"rb")
@@ -26,17 +40,23 @@ def write_to_rain_basefs(rf3_name):
         nc_file.close()
     except FileNotFoundError : 
         print(f"Error: {rf3_name} not found")
-        return 0
+        return None
 
     # get the metadata
     fname = os.path.basename(rf3_name)
     mean, std, war = get_field_stats(buf)
     
     # setup the database and write  
-    client = MongoClient()
-    rain_base = client["rain_base_fs"]
-    fs = gridfs.GridFSBucket(rain_base) 
-    file_id = fs.upload_from_stream(fname,buf,metadata={"mean":mean,"stdev":std,"WAR":war})
-    client.close()
+    if db_name != None: 
+        client = MongoClient()
+        db_client = client[db_name]
+        fs = gridfs.GridFSBucket(db_client) 
+        file_id = fs.upload_from_stream(fname,buf,metadata={"name":"rain_rate","mean":mean,"stdev":std,"WAR":war})
+        client.close()
+    else:
+        fs = gridfs.GridFSBucket(db_client) 
+        file_id = fs.upload_from_stream(fname,buf,metadata={"name":"rain_rate","mean":mean,"stdev":std,"WAR":war})
+
+    print(f"written {rf3_name}")
     return file_id
 
