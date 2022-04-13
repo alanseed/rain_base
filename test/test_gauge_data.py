25e9c6006b75cbdade08904ea9795e19769f07d3
alan.seed <alan.seed@gmail.com>
2022-04-13 11:06:19 +1000

    Modified test

--- test/test_gauge_data.py
+++ test/test_gauge_data.py
@@ -1,114 +1,120 @@
-# Test reading the rainfields gauge netcdf files and writing the contents to a database 
+""" Test reading a set of rainfields3 gauge obs files into the database
 
-from pymongo import MongoClient
+    Returns:
+        _type_: _description_
+"""
+
+
 import os
-import datetime 
-import netCDF4 as nc  
-from geojson import Point 
+import datetime
+from netCDF4 import Dataset
+from pymongo import MongoClient
+from geojson import Point
+
+START_DATE = datetime.datetime(2021, 12, 7, 21, 0)
+END_DATE = datetime.datetime(2021, 12, 7, 23, 45)
+TIME_STEP = datetime.timedelta(minutes=15)
+BASE_DIR = "/home/awseed/data/RF3/gaugeobs/0"
+PRODUCT = "gaugeobs.nc"
+STN_ID = 0
+DB_NAME = 'gaugeobs'
+TEST_PATH = "/home/awseed/data/RF3/gaugeobs/0/2021/12/07/0_20211207_210000.gaugeobs.nc"
 
 # Write the data to the MongoDB
-# returns None on error 
-def load_gauge_data(**kwargs): 
-    rf3_name = kwargs.get("file_path",None)
-    db = kwargs.get("db",None)
-    db_name = kwargs.get("db_name", None) 
-    if db == None and db_name == None:
+# returns None on error
+
+
+def load_gauge_data(**kwargs):
+    """Read a rainfields3 rain gauge netcdf file and write the data to the database 
+
+    Returns:
+        None: on error 
+        0: on success 
+    """
+    rf3_name = kwargs.get("file_path", None)
+    gauge_db_client = kwargs.get("db_client", None)
+    gauge_db_name = kwargs.get("db_name", None)
+    if gauge_db_client is None and gauge_db_name is None:
         print("Need one of db_name or db_client")
         return None
-    if rf3_name ==  None: 
-        print(f"file_path keyword not found")
+    if rf3_name is None:
+        print("file_path keyword not found")
         return None
 
-    # open the ncfile 
+    # open the ncfile
     try:
-        ds = nc.Dataset(rf3_name)        
-    except FileNotFoundError : 
+        rf3_ds = Dataset(rf3_name)
+    except FileNotFoundError:
         print(f"Error: {rf3_name} not found")
         return None
 
-    # read in the variables 
-    number_stations = ds.dimensions['stations'].size 
-    print(f"found {number_stations} stations") 
+    # read in the variables
+    number_stations = rf3_ds.dimensions['stations'].size
+    print(f"found {number_stations} stations")
+    rf3_valid_time = rf3_ds['valid_time'][0].item()
 
-    start_time = ds['start_time'][0].item()
-    valid_time = ds['valid_time'][0].item()
-    
-    station_id = ds['station_id'][:]
-    latitude = ds['latitude'][:]
-    longitude = ds['longitude'][:]
-    precipitation = ds['precipitation'][:]
-    latest_obs_time = ds['latest_obs_time'][:]
+    station_id = rf3_ds['station_id'][:]
+    latitude = rf3_ds['latitude'][:]
+    longitude = rf3_ds['longitude'][:]
+    precipitation = rf3_ds['precipitation'][:]
+    latest_obs_time = rf3_ds['latest_obs_time'][:]
 
-    # loop over the stations and load up the gauge_list 
+    # loop over the stations and load up the gauge_list
     gauge_list = []
-    for ia in range (1,number_stations): 
+    for ia in range(1, number_stations):
         lat = latitude[ia].item()
-        lon = longitude[ia].item() 
-        loc = Point([lon,lat])
+        lon = longitude[ia].item()
+        loc = Point([lon, lat])
         record = {
-            'start_time':start_time,
-            'valid_time':valid_time,
-            'station_id':station_id[ia].item(),
-            'precipitation':precipitation[ia].item(),
-            'latest_obs_time':latest_obs_time[ia].item(),
-            'location':loc
+            'start_time': start_time,
+            'valid_time': rf3_valid_time,
+            'station_id': station_id[ia].item(),
+            'precipitation': precipitation[ia].item(),
+            'latest_obs_time': latest_obs_time[ia].item(),
+            'location': loc
         }
         gauge_list.append(record)
 
-    # write to the database 
-    if db_name != None: 
+    # write to the database
+    if gauge_db_name is not None:
         myclient = MongoClient()
-        db = myclient[db_name]
-    gauge_collection = db["gauge_data"] 
+        gauge_db_client = myclient[gauge_db_name]
+    gauge_collection = gauge_db_client["gauge_data"]
     gauge_collection.insert_many(gauge_list)
 
-    if db_name != None: 
+    if gauge_db_name is not None:
         myclient.close()
 
     print(f"written {rf3_name}")
     return 0
 
 
-# import gauge_data 
-
-# traverse the file directories and write the data out to the database 
-start_date = datetime.datetime(2021,12,7) 
-end_date = datetime.datetime(2021,12,8)
-time_step = datetime.timedelta(minutes=15)
-base_dir = "/home/awseed/data/RF3/gaugeobs/0"
-product = "gaugeobs.nc"
-stn_id = 0 
-start_time = datetime.datetime.now() 
-db_name = 'gaugeobs'
-test_path = "/home/awseed/data/RF3/gaugeobs/0/2021/12/07/0_20211207_210000.gaugeobs.nc"
-
-
 # drop the existing data base for the test
-client = MongoClient() 
-dbnames = client.list_database_names() 
-if db_name in dbnames: 
-    client.drop_database(db_name)
+client = MongoClient()
+dbnames = client.list_database_names()
+if DB_NAME in dbnames:
+    client.drop_database(DB_NAME)
 
-db = client[db_name]
-file_number = 0 
-vtime = start_date
-while vtime < end_date:
-    y = vtime.strftime("%Y") 
+db_client = client[DB_NAME]
+vtime = START_DATE
+file_number = 0
+start_time = datetime.datetime.now()
+while vtime < END_DATE:
+    y = vtime.strftime("%Y")
     ymd = vtime.strftime("%Y%m%d")
     hms = vtime.strftime("%H%M%S")
-    file_path = os.path.join(base_dir,vtime.strftime("%Y/%m/%d/"))
-    file_name = f"{stn_id}_{ymd}_{hms}.{product}"
-    file_path = os.path.join(file_path, file_name) 
+    file_path = os.path.join(BASE_DIR, vtime.strftime("%Y/%m/%d/"))
+    file_name = f"{STN_ID}_{ymd}_{hms}.{PRODUCT}"
+    file_path = os.path.join(file_path, file_name)
     print(f"file path = {file_path}")
-    file_id = load_gauge_data(file_path=test_path,db=db)
-    if file_id == None:
-        break 
+    file_id = load_gauge_data(file_path=file_path, db_client=db_client)
+    if file_id is None:
+        break
 
-    # vtime = vtime + time_step  
-    vtime = end_date  
+    vtime = vtime + TIME_STEP
     file_number += 1
 
-end_time = datetime.datetime.now() 
-dt = end_time - start_time 
+end_time = datetime.datetime.now()
+dt = end_time - start_time
 print(f"Processed {file_number} files in {dt}")
 client.close()
