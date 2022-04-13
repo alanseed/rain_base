01b6d7b0d35be23c4b9c5861332b727a28ac60f0
alan.seed <alan.seed@gmail.com>
2022-04-13 11:49:05 +1000

    WIP 130422

--- src/gauge_data.py
+++ src/gauge_data.py
@@ -1,44 +1,73 @@
+"""Module to read and write gauge data to the database 
 
-import gridfs
+    Returns:
+        _type_: _description_
+"""
 from pymongo import MongoClient
-# Function to read the rainfields gauge data and write it to the database 
-import netCDF4 as nc
-import os
+from netCDF4 import Dataset
+from geojson import Point
 
-# Write the data to the MongoDB
-# returns None on error 
-def load_gauge_data(**kwargs): 
-    rf3_name = kwargs.get("file_path",None)
-    db_client = kwargs.get("db_client",None)
-    db_name = kwargs.get("db_name", None) 
-    if db_client == None and db_name == None:
+def load_gauge_data(**kwargs):
+    """Read a rainfields3 rain gauge netcdf file and write the data to the database 
+
+    Returns:
+        None: on error 
+        0: on success 
+    """
+    rf3_name = kwargs.get("file_path", None)
+    gauge_db_client = kwargs.get("db_client", None)
+    gauge_db_name = kwargs.get("db_name", None)
+    if gauge_db_client is None and gauge_db_name is None:
         print("Need one of db_name or db_client")
         return None
-
-    if rf3_name ==  None: 
-        print(f"file_path keyword not found")
+    if rf3_name is None:
+        print("file_path keyword not found")
         return None
 
-    # open the ncfile 
+    # open the ncfile
     try:
-        ds = nc.Dataset(rf3_name)
-        print(ds)
-        
-    except FileNotFoundError : 
+        rf3_ds = Dataset(rf3_name)
+    except FileNotFoundError:
         print(f"Error: {rf3_name} not found")
         return None
 
-    # # setup the database and write  
-    # if db_name != None: 
-    #     client = MongoClient()
-    #     db_client = client[db_name]
-    #     fs = gridfs.GridFSBucket(db_client) 
-    #     file_id = fs.upload_from_stream(fname,buf,metadata={"name":"rain_rate","mean":mean,"stdev":std,"WAR":war})
-    #     client.close()
-    # else:
-    #     fs = gridfs.GridFSBucket(db_client) 
-    #     file_id = fs.upload_from_stream(fname,buf,metadata={"name":"rain_rate","mean":mean,"stdev":std,"WAR":war})
+    # read in the variables
+    number_stations = rf3_ds.dimensions['stations'].size
+    print(f"found {number_stations} stations")
+    rf3_start_time = rf3_ds['start_time'][0].item()
+    rf3_valid_time = rf3_ds['valid_time'][0].item()
+
+    station_id = rf3_ds['station_id'][:]
+    latitude = rf3_ds['latitude'][:]
+    longitude = rf3_ds['longitude'][:]
+    precipitation = rf3_ds['precipitation'][:]
+    latest_obs_time = rf3_ds['latest_obs_time'][:]
+
+    # loop over the stations and load up the gauge_list
+    gauge_list = []
+    for ia in range(1, number_stations):
+        lat = latitude[ia].item()
+        lon = longitude[ia].item()
+        loc = Point([lon, lat])
+        record = {
+            'start_time': rf3_start_time,
+            'valid_time': rf3_valid_time,
+            'station_id': station_id[ia].item(),
+            'precipitation': precipitation[ia].item(),
+            'latest_obs_time': latest_obs_time[ia].item(),
+            'location': loc
+        }
+        gauge_list.append(record)
+
+    # write to the database
+    if gauge_db_name is not None:
+        myclient = MongoClient()
+        gauge_db_client = myclient[gauge_db_name]
+    gauge_collection = gauge_db_client["gauge_data"]
+    gauge_collection.insert_many(gauge_list)
+
+    if gauge_db_name is not None:
+        myclient.close()
 
     print(f"written {rf3_name}")
     return 0
-
--- test/test_gauge_data.py
+++ test/test_gauge_data.py
@@ -4,12 +4,14 @@
         _type_: _description_
 """
 
+import sys
+# adding src to the system path
+sys.path.insert(0,'../src')
+from gauge_data import load_gauge_data
 
 import os
 import datetime
-from netCDF4 import Dataset
 from pymongo import MongoClient
-from geojson import Point
 
 START_DATE = datetime.datetime(2021, 12, 7, 21, 0)
 END_DATE = datetime.datetime(2021, 12, 7, 23, 45)
@@ -19,74 +21,6 @@ PRODUCT = "gaugeobs.nc"
 STN_ID = 0
 DB_NAME = 'gaugeobs'
 TEST_PATH = "/home/awseed/data/RF3/gaugeobs/0/2021/12/07/0_20211207_210000.gaugeobs.nc"
-
-# Write the data to the MongoDB
-# returns None on error
-
-
-def load_gauge_data(**kwargs):
-    """Read a rainfields3 rain gauge netcdf file and write the data to the database 
-
-    Returns:
-        None: on error 
-        0: on success 
-    """
-    rf3_name = kwargs.get("file_path", None)
-    gauge_db_client = kwargs.get("db_client", None)
-    gauge_db_name = kwargs.get("db_name", None)
-    if gauge_db_client is None and gauge_db_name is None:
-        print("Need one of db_name or db_client")
-        return None
-    if rf3_name is None:
-        print("file_path keyword not found")
-        return None
-
-    # open the ncfile
-    try:
-        rf3_ds = Dataset(rf3_name)
-    except FileNotFoundError:
-        print(f"Error: {rf3_name} not found")
-        return None
-
-    # read in the variables
-    number_stations = rf3_ds.dimensions['stations'].size
-    print(f"found {number_stations} stations")
-    rf3_valid_time = rf3_ds['valid_time'][0].item()
-
-    station_id = rf3_ds['station_id'][:]
-    latitude = rf3_ds['latitude'][:]
-    longitude = rf3_ds['longitude'][:]
-    precipitation = rf3_ds['precipitation'][:]
-    latest_obs_time = rf3_ds['latest_obs_time'][:]
-
-    # loop over the stations and load up the gauge_list
-    gauge_list = []
-    for ia in range(1, number_stations):
-        lat = latitude[ia].item()
-        lon = longitude[ia].item()
-        loc = Point([lon, lat])
-        record = {
-            'start_time': start_time,
-            'valid_time': rf3_valid_time,
-            'station_id': station_id[ia].item(),
-            'precipitation': precipitation[ia].item(),
-            'latest_obs_time': latest_obs_time[ia].item(),
-            'location': loc
-        }
-        gauge_list.append(record)
-
-    # write to the database
-    if gauge_db_name is not None:
-        myclient = MongoClient()
-        gauge_db_client = myclient[gauge_db_name]
-    gauge_collection = gauge_db_client["gauge_data"]
-    gauge_collection.insert_many(gauge_list)
-
-    if gauge_db_name is not None:
-        myclient.close()
-
-    print(f"written {rf3_name}")
-    return 0
 
 
 # drop the existing data base for the test
@@ -106,7 +40,6 @@ while vtime < END_DATE:
     file_path = os.path.join(BASE_DIR, vtime.strftime("%Y/%m/%d/"))
     file_name = f"{STN_ID}_{ymd}_{hms}.{PRODUCT}"
     file_path = os.path.join(file_path, file_name)
-    print(f"file path = {file_path}")
     file_id = load_gauge_data(file_path=file_path, db_client=db_client)
     if file_id is None:
         break
